ce453fb8127e9f8f0b455260647fd0da8e0a68db
Andre <itstheandre@gmail.com>
2020-10-13 20:05:56 +0200

    Starting code

--- code-along/server/routes/upload.js
+++ code-along/server/routes/upload.js
@@ -5,7 +5,5 @@ router.get("/", (req, res) => res.json(false));
 
 router.post("/image", uploadCloud.single("image"), (req, res) => {
   console.log(req.file);
-  res.json(true);
 });
-
 module.exports = router;
